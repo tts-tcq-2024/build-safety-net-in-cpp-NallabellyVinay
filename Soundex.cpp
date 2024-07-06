89a67cdaddc0059391686db4ee41457867c53c63
NallabellyVinay <52525335+NallabellyVinay@users.noreply.github.com>
2024-07-07 03:05:08 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -2,40 +2,28 @@
 #include <cctype>
 #include <string>
 
+// Function to get the Soundex code for a given character
 char getSoundexCode(char c) {
+    static const std::string codes = "01230120022455012623010202";
     c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
-    }
-}
-
-std::string padSoundex(const std::string& soundex) {
-    std::string paddedSoundex = soundex;
-    while (paddedSoundex.length() < 4) {
-        paddedSoundex += '0';
-    }
-    return paddedSoundex;
+    return (c >= 'A' && c <= 'Z') ? codes[c - 'A'] : '0';
 }
 
 std::string generateSoundex(const std::string& name) {
     if (name.empty()) return "";
 
     std::string soundex(1, toupper(name[0]));
-    char prevCode = getSoundexCode(name[0]);
-
-    for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
+    char prevCode = getSoundexCode(soundex[0]);
+    size_t i = 1;
+    while (i < name.length() && soundex.length() < 4) {
         char code = getSoundexCode(name[i]);
         if (code != '0' && code != prevCode) {
             soundex += code;
             prevCode = code;
         }
+        ++i;
     }
 
-    return padSoundex(soundex);
+    soundex.resize(4, '0');
+    return soundex;
 }
