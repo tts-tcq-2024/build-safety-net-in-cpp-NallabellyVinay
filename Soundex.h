13b7b79842b4c22e8a2516c9eda7e296f4fcb51c
NallabellyVinay <52525335+NallabellyVinay@users.noreply.github.com>
2024-07-07 15:39:28 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,8 +1,9 @@
-#ifndef SOUNDEX_H
-#define SOUNDEX_H
+#ifndef HEADER_H
+#define HEADER_H
 
 #include <string>
 
+char getSoundexCode(char c);
 std::string generateSoundex(const std::string& name);
 
-#endif // SOUNDEX_H
+#endif // HEADER_H
